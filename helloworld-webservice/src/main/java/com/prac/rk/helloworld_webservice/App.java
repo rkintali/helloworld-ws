445b4bb5079a32376d793e6cb08b87c999ddf648
TU <user@testdomain.com>
2016-06-25 09:20:39 +1000

    Initial commit

---
+++ helloworld-webservice/src/main/java/com/prac/rk/helloworld_webservice/App.java
@@ -0,0 +1,13 @@
+package com.prac.rk.helloworld_webservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
